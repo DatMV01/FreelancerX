a9e8ec939956fffad04cb51eb0f7c1865c137985
datmvv <datmv01@gmail.com>
2025-02-05 16:18:42 +0700

    feat: add user seeding functionality to the database

--- backend/src/database/seeds/relational/seed.module.ts
+++ backend/src/database/seeds/relational/seed.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeORMModule } from 'src/database/typeorm/typeorm.module';
 import { RoleSeedModule } from './role/role.seed.module';
 import { StatusSeedModule } from './status/status.seed.module';
+import { UserSeedModule } from './user/user.seed.module';
 
 @Module({
-  imports: [TypeORMModule, RoleSeedModule, StatusSeedModule],
+  imports: [TypeORMModule, RoleSeedModule, StatusSeedModule, UserSeedModule],
 })
 export class SeedModule {}
---
+++ backend/src/database/seeds/relational/user/user.seed.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcryptjs';
+import { RoleEnum } from 'src/modules/roles/roles.enum';
+import { StatusEnum } from 'src/modules/status/enum/statuses.enum';
+import { UserEntity } from 'src/modules/users/entities/user.entity';
+
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserSeedService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private repository: Repository<UserEntity>,
+  ) {}
+
+  async run() {
+    const users: Partial<UserEntity>[] = [
+      {
+        firstName: 'Super',
+        lastName: 'Admin',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('admin123', 10),
+        role: {
+          id: RoleEnum.ADMIN,
+        } as any,
+        status: {
+          id: StatusEnum.ACTIVE,
+        } as any,
+      },
+    ];
+
+    for (let index = 1; index <= 500; index++) {
+      users.push({
+        email: `user${index}@example.com`,
+        firstName: `user${index} firstName`,
+        lastName: `user${index} lastName`,
+        password: bcrypt.hashSync('user123', 10),
+        role: {
+          id: Math.floor(Math.random() * 3 + 2),
+        } as any,
+        status: {
+          id: Math.floor(Math.random() * 4 + 1),
+        } as any,
+      });
+    }
+
+    await this.repository.clear();
+    await this.repository.save(users);
+  }
+}
