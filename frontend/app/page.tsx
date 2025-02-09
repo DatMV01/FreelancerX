da26dbd4d4f538e3b412da4956df21c531d2d213
datmvv <datmv01@gmail.com>
2025-02-09 19:19:03 +0700

    feat: initialize Next.js frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-items-center min-h-screen p-auto font-[family-name:var(--font-geist-sans)]">
+      <p>Hello World</p>
+    </div>
+  );
+}
