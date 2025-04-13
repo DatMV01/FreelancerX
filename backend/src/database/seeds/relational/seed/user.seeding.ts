780a9b895490377973924de9b32d1c65649bca8d
datmvv <datmv01@gmail.com>
2025-04-13 09:22:33 +0700

    feat: update user seeding logic to create distinct user roles and profiles

--- backend/src/database/seeds/relational/seed/user.seeding.ts
+++ backend/src/database/seeds/relational/seed/user.seeding.ts
@@ -23,11 +23,47 @@ export class UserSeeding {
     await this.repository.clear();
     await this.repository.query('SET FOREIGN_KEY_CHECKS=1;');
 
-    const reelancerAdmin = this.freelancerRepository.create({
+    const adminUser = {
+      id: 'cf946efc-e04b-49b1-a547-268c69d3ceef',
+      fullName: 'Super Admin',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('user123', 10),
+      avatar: faker.image.avatar(),
+      country: 'Vietnam',
+      phone: faker.phone.number(),
+      roleId: RoleEnum.ADMIN,
+      statusId: StatusEnum.ACTIVE,
+    };
+
+    const buyerUser = {
+      id: 'ea3204bf-1c46-413b-8626-7b10be7f7953',
+      fullName: 'Buyer',
+      email: 'buyer@example.com',
+      password: bcrypt.hashSync('user123', 10),
+      avatar: faker.image.avatar(),
+      country: 'Vietnam',
+      phone: faker.phone.number(),
+      roleId: RoleEnum.BUYER,
+      statusId: StatusEnum.ACTIVE,
+    };
+
+    const freelancerUser = {
+      id: 'b9686ad5-bf69-49d3-850e-0b5395a2bc52',
+      fullName: 'Freelancer',
+      email: 'freelancer@example.com',
+      password: bcrypt.hashSync('user123', 10),
+      avatar: faker.image.avatar(),
+      country: 'Vietnam',
+      phone: faker.phone.number(),
+      roleId: RoleEnum.FREELANCER,
+      statusId: StatusEnum.ACTIVE,
+    };
+
+    const freelancerProfile = this.freelancerRepository.create({
       id: '91bfe180-9448-4b32-84ae-a6b8a6d99b84',
-      userId: 'cf946efc-e04b-49b1-a547-268c69d3ceef',
-      email: 'mamotgio@gmail.com',
-      fullName: 'Mai Dat',
+      userId: 'b9686ad5-bf69-49d3-850e-0b5395a2bc52',
+      email: 'freelancer@example.com',
+      fullName: 'Freelancer',
       phone: '0818012377',
       country: 'Ecuador',
       bio: 'Expert mobile app developer',
@@ -62,25 +98,7 @@ export class UserSeeding {
       ],
     } as any);
 
-    const users: Partial<UserEntity>[] = [
-      {
-        id: 'cf946efc-e04b-49b1-a547-268c69d3ceef',
-        fullName: 'Super Admin',
-        email: 'admin@example.com',
-        password: bcrypt.hashSync('user123', 10),
-        avatar: faker.image.avatar(),
-        country: 'Vietnam',
-        phone: faker.phone.number(),
-        roleId: RoleEnum.ADMIN,
-        statusId: StatusEnum.ACTIVE,
-        // role: {
-        //   id: RoleEnum.ADMIN,
-        // } as any,
-        // status: {
-        //   id: StatusEnum.ACTIVE,
-        // } as any,
-      },
-    ];
+    const users: Partial<UserEntity>[] = [adminUser, buyerUser, freelancerUser];
 
     for (let index = 1; index <= 50; index++) {
       users.push({
@@ -92,18 +110,12 @@ export class UserSeeding {
         phone: faker.phone.number(),
         roleId: Math.floor(Math.random() * 4 + 1),
         statusId: Math.floor(Math.random() * 5 + 1),
-        // role: {
-        //   id: Math.floor(Math.random() * 4 + 1),
-        // } as any,
-        // status: {
-        //   id: Math.floor(Math.random() * 5 + 1),
-        // } as any,
       });
     }
 
     await this.repository.save(users);
 
-    await this.freelancerRepository.save(reelancerAdmin);
+    await this.freelancerRepository.save(freelancerProfile);
 
     console.log('\n == Users are seeded completely !!! == \n');
   }
